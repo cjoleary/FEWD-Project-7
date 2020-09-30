9abc8de8994b7d530a137c18fd393aecbf2df196
cjoleary <cjoleary17@gmail.com>
2020-09-30 14:32:09 -0600

    Update dash.js

--- js/dash.js
+++ js/dash.js
@@ -15,6 +15,6 @@ const alertBanner = document.getElementById("alert");
     alertBanner.addEventListener('click', e => {
         const element = e.target;
         if (element.classList.contains("alert-banner-close")) {
-        alertBanner.style.display = "none"
+            alertBanner.style.display = "none"
         }
     });
