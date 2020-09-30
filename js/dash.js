b16ec6074cae9d9011bf0e8cc2154048e131fe0d
cjoleary <cjoleary17@gmail.com>
2020-09-30 14:41:40 -0600

    added delay to alert banner close

--- js/dash.js
+++ js/dash.js
@@ -15,6 +15,6 @@ const alertBanner = document.getElementById("alert");
     alertBanner.addEventListener('click', e => {
         const element = e.target;
         if (element.classList.contains("alert-banner-close")) {
-            alertBanner.style.display = "none"
+            setTimeout( function () {alertBanner.style.display = "none"}, 250);
         }
     });
