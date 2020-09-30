8203286efe056c45ad1b44c67a72133b813df854
cjoleary <cjoleary17@gmail.com>
2020-09-30 14:11:52 -0600

    created alert banner

---
+++ js/dash.js
@@ -0,0 +1,20 @@
+
+
+// alert banner
+const alertBanner = document.getElementById("alert");
+    // create the html for the banner
+    alertBanner.innerHTML =
+        `
+        <div class="alert-banner">
+        <p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks
+        to complete</p>
+        <p class="alert-banner-close">x</p>
+        </div>
+        `
+    // alert banner event listener
+    alertBanner.addEventListener('click', e => {
+        const element = e.target;
+        if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none"
+        }
+    });
