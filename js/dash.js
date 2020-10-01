d5b284c7a4f2817c805ba29a6b62250b190b042b
cjoleary <cjoleary17@gmail.com>
2020-10-01 16:58:27 -0600

    added functionality to message user widget

--- js/dash.js
+++ js/dash.js
@@ -125,3 +125,24 @@ const mobileCanvas = document.getElementById("mobileChart");
         data: mobileData,
         options: mobileOptions
     });
+
+// message user widget
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+    // send button click event listener
+    send.addEventListener('click', () => {
+        // ensure user and message fields are filled out
+        if (user.value === "" && message.value === "") {
+            alert("Please fill out user and message fields before sending");
+        } else if (user.value === "" ) {
+            alert("Please fill out user field before sending");
+        } else if (message.value === "" ) {
+            alert("Please fill out message field before sending");
+        } else {
+            alert(`Message successfully sent to: ${user.value}`);
+            user.value = "";
+            message.value = "";
+        }
+    });
